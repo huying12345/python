0f8a8f43cc12afadd3dfb1984468bc4436216f2e
Candy <candy_2446376952@163.com>
2019-11-07 10:05:34 +0800

    Create class.py

---
+++ class.py
@@ -0,0 +1,13 @@
+# Student对象的gender字段对外隐藏起来，用get_gender()和set_gender()代替，并检查参数有效性：
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+    def get_gender(self):
+        return self.__gender
+    def set_gender(self, gender):
+        if gender!='male' and gender!='female':
+            raise ValueError('error gender')
+        else:
+            self.__gender = gender
+
